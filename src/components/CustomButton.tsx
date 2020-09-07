49f208d67751a369757cf3ed4a18d138e8e943b4
Anirudh Agarwal <anirudh.agarwal@flipkart.com>
2020-09-07 14:40:20 +0530

    Added custom Button

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "./Button";
+import { View } from "react-native";
+
+export class CustomButton extends Button {
+  render() {
+    return <View />;
+  }
+}
